6c641479364c14cfce50a122d9f02e6e69baefc6
神塚佑太 <cybor_ggg@icloud.com>
2022-02-13 18:53:52 +0900

    ScheduleAdapter内、getItemCount、ScheduleHolder、onBindViewHolderにLog.dを追加し確認。

--- app/src/main/java/com/example/scheduleapp/ScheduleAdapter.kt
+++ app/src/main/java/com/example/scheduleapp/ScheduleAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.scheduleapp
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,7 @@ class ScheduleAdapter(var list: MutableList<Task>) : RecyclerView.Adapter<Schedu
     //1行分のレイアウトを生成
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduleHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_line, parent, false)
+        Log.println(Log.DEBUG, itemView.toString(), "DEBUG")
         return ScheduleHolder(itemView)
     }
 
@@ -16,8 +18,13 @@ class ScheduleAdapter(var list: MutableList<Task>) : RecyclerView.Adapter<Schedu
     override fun onBindViewHolder(holder: ScheduleHolder, position: Int) {
         holder.titleList.text = list[position].title
         holder.deadlineList.text = list[position].deadline
+        Log.println(Log.DEBUG, list.toString(), "DEBUG")
     }
 
     //getItemCountは行の数をカウント
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int{
+        val size = list.size
+        Log.println(Log.DEBUG, size.toString(), "DEBUG")
+        return size
+    }
 }
